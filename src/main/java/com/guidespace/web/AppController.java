54ac30b55e80f74f06aeddcd2765c5e6c3f984f3
Leonidk29 <leonid.rossenkov@gmail.com>
2016-09-22 02:33:58 +0300

    Basic Java Spring Boot project

---
+++ src/main/java/com/guidespace/web/AppController.java
@@ -0,0 +1,20 @@
+package com.guidespace.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Leonid29 on 22.09.2016.
+ */
+
+@Controller
+public class AppController {
+
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello Boyas!";
+    }
+}
