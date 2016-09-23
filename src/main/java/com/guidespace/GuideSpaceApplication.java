8ca75e2edde850b233bcf6a847eccf0cc371fbca
VladAlenitsev <vlad.alenitsev@gmail.com>
2016-09-23 14:03:20 +0300

    testing

--- src/main/java/com/guidespace/GuideSpaceApplication.java
+++ src/main/java/com/guidespace/GuideSpaceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GuideSpaceApplication {
 //useless comment
+//HUEHUEHUE
 	public static void main(String[] args) {
 		SpringApplication.run(GuideSpaceApplication.class, args);
 	}
