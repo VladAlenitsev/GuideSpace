c3e3fea3451e87a7c959ae98eca266c41724e001
Leonidk29 <leonid.rossenkov@gmail.com>
2016-11-02 16:39:25 +0200

    Implementing system to give user rights, still in development.

--- src/main/java/com/guidespace/web/AppController.java
+++ src/main/java/com/guidespace/web/AppController.java
@@ -1,6 +1,7 @@
 package com.guidespace.web;
 
 import com.guidespace.domain.ExamQuestion;
+import com.guidespace.domain.Person;
 import com.guidespace.service.DuplicateEmailException;
 import com.guidespace.service.DuplicateUsernameException;
 import com.guidespace.service.ExamQuestionService;
@@ -8,6 +9,7 @@ import com.guidespace.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
-import java.util.List;
 
 
 @Controller
@@ -46,6 +47,31 @@ public class AppController {
                 !(authentication instanceof AnonymousAuthenticationToken);
     }
 
+
+    @RequestMapping(value = "/isAdmin", method = RequestMethod.GET)
+    @ResponseBody
+    public Boolean isAdmin() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getAuthorities().contains(new SimpleGrantedAuthority("ADMIN"));
+    }
+
+    @RequestMapping(value = "/giveAdmin", method = RequestMethod.GET)
+    @ResponseBody
+    public String isAuthenticated2() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Person leo = userService.getUser(authentication.getName());
+        leo.setUser_role_id(2);
+        userService.update(leo);
+        return authentication.getAuthorities().toString();
+    }
+
+    @RequestMapping(value = "/isTest", method = RequestMethod.GET)
+    @ResponseBody
+    public String isAuthenticated3() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getAuthorities().toString();
+    }
+
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     @ResponseBody
     public void authenticate(String username, String password, String email) throws DuplicateEmailException, DuplicateUsernameException {
@@ -64,6 +90,17 @@ public class AppController {
         ArrayList<String> result = new ArrayList<String>();
         for (ExamQuestion eq: examQuestionService.getQuestions()){
             result.add(eq.getQuestion());
+        }
+        return result;
+    }
+
+
+    @RequestMapping(value = "/getUsers", method = {RequestMethod.GET}, produces = "application/json; charset=UTF-8")
+    @ResponseBody
+    public ArrayList<String> getUsers() {
+        ArrayList<String> result = new ArrayList<String>();
+        for (Person eq: userService.getUsers()){
+            result.add(eq.getUsername());
         }
         return result;
     }
