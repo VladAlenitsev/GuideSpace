d944d4fdf42d30f1afe7ccb5ac408f13aa7a0070
VladAlenitsev <vlad.alenitsev@gmail.com>
2016-11-07 05:04:46 +0200

    Corrections for circleci testing

--- src/test/java/com/guidespace/UITests.java
+++ src/test/java/com/guidespace/UITests.java
@@ -16,7 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
  * Created by Vitali on 11/2/2016.
  */
 @Transactional
-public class UI {
+public class UITests {
 
     private static WebDriver webDriver;
     private static long randNum;
