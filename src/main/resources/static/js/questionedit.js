64439d4ab53ef50c4a9db01dbfe05a3d3513a443
vtlkzmn <vtlkzmn@gmail.com>
2016-12-04 19:41:53 +0200

    2

--- src/main/resources/static/js/questionedit.js
+++ src/main/resources/static/js/questionedit.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
     var search = $('#submitsearch');
-    var searchText = search.text;
+    var searchText = $('#searchtext').text;
     alert(searchText);
     search.click(function() {
-
+        $.ajax({
+            url: "/getAllQuestionsWithAnswers"
+            success: function(result) {
+            console.log(result);
+                 for (var i = 0, keys = Object.keys(result), ii = keys.length; i < ii; i++) {
+                    console.log(keys);
+                    console.log('key : ' + keys[i] + ' val : ' + result[keys[i]]);
+                 }
+            }
+        });
     });
 });
