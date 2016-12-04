0a91f85349a7d0eba2dbcb36a360a63e820563bd
NorthernSenpai <rioschults@hotmail.com>
2016-12-04 16:01:51 +0200

    UI Tests parandatud

---
+++ src/main/resources/static/js/examreg.js
@@ -0,0 +1,136 @@
+$(document).ready(function() {
+
+    $("#startdate").datetimepicker({
+        format:'d-m-Y H:i',
+    	minDate:'0',
+    	dayOfWeekStart:1
+    });
+    $("#enddate").datetimepicker({
+        format:'d-m-Y H:i',
+    	minDate:'0',
+    	dayOfWeekStart:1
+    });
+
+    $.ajax({url: "/getClassificators", success: function(result){
+        for (i = 0; i < result.length; i++) {
+            var opt = document.createElement('option');
+            opt.value = result[i].id;
+            opt.innerHTML = result[i].classif_name;
+            $('#classifSelect').append(opt);
+        }
+    }});
+
+    refresh();
+
+    $('#examreg').click(function(){
+        var cookie = JSON.parse($.cookie('CSRF'));
+        var map = getExamData();
+        $.ajax({
+            data: JSON.stringify(map),
+            headers: {'X-CSRF-TOKEN': cookie.csrf},
+            contentType: 'application/json',
+            timeout: 5000,
+            type: 'POST',
+            url: '/addExamination',
+            success: function(data){
+                $.notify("Exam has been added successfully", "success");
+                location.reload();
+            },
+            error: function(errorThrown){
+               console.log(errorThrown)
+               $.notify("Error while trying to create new examination.", "error");
+
+            }
+        })
+    });
+    $('#examopen').click(function(){
+        var cookie = JSON.parse($.cookie('CSRF'));
+        var examid = {'id': $('#examSelect').val()}
+        $.ajax({
+            data: JSON.stringify(examid),
+            headers: {'X-CSRF-TOKEN': cookie.csrf},
+            contentType: 'application/json',
+            timeout: 5000,
+            type: 'POST',
+            url: '/startExamination',
+            success: function(data){
+                $.notify("Exam has been started.", "success");
+
+            },
+            error: function(errorThrown){
+                console.log(errorThrown)
+                $.notify("Couldn't start the exam.", "error");
+
+            }
+        })
+    });
+
+    $('#examclose').click(function(){
+        var cookie = JSON.parse($.cookie('CSRF'));
+        var examid = {'id': $('#examSelect').val()}
+        $.ajax({
+            data: JSON.stringify(examid),
+            headers: {'X-CSRF-TOKEN': cookie.csrf},
+            contentType: 'application/json',
+            timeout: 5000,
+            type: 'POST',
+            url: '/closeExamination',
+            success: function(data){
+                document.getElementById("outputDiv").innerHTML = "Exam has been closed.";
+            },
+            error: function(errorThrown){
+                console.log(errorThrown)
+                document.getElementById("outputDiv").innerHTML = "Couldn't close the exam.";
+            }
+        })
+    });
+
+    //not implemented
+    $('#examdelete').click(function(){
+        var cookie = JSON.parse($.cookie('CSRF'));
+        var examid = {'id': $('#examSelect').val()}
+        $.ajax({
+            data: JSON.stringify(examid),
+            headers: {'X-CSRF-TOKEN': cookie.csrf},
+            contentType: 'application/json',
+            timeout: 5000,
+            type: 'POST',
+            url: '/deleteExamination',
+            success: function(data){
+                location.reload();
+            },
+            error: function(errorThrown){
+                console.log(errorThrown)
+                document.getElementById("outputDiv").innerHTML = "Couldn't delete the exam.";
+            }
+        })
+    });
+
+    $('#headers').load('/html/components/header.html');
+});
+
+function getExamData(){
+    return {'startdate': $('#startdate').val(),'enddate': $('#enddate').val(),'classif':$('#classifSelect').val()}
+}
+
+function refresh(){
+    var select = document.getElementById("examSelect");
+    var length = select.options.length;
+    for (i = 0; i < length; i++) {
+      select.options[i] = null;
+    }
+    var opt = document.createElement('option');
+    opt.innerHTML = "Created examinations.."
+    $('#examSelect').append(opt);
+    $.ajax({url: "/getExaminations", success: function(result){
+        for (i = 0; i < result.length; i++) {
+            var m = result[i]
+            var opt = document.createElement('option');
+            opt.value = m['id'];
+            var d1 = m['startdate'].slice(0,-5);
+            var d2 = m['enddate'].slice(0,-5);
+            opt.innerHTML = d1 + " to " + d2 + "; Question catergory: " + m['classif_name'];
+            $('#examSelect').append(opt);
+        }
+    }});
+}
--- src/test/java/com/guidespace/UI.java
+++ src/test/java/com/guidespace/UI.java
@@ -27,44 +27,43 @@ public class UI {
         System.setProperty("webdriver.chrome.driver", "./src/test/java/com/guidespace/chromedriver.exe");
         webDriver = new ChromeDriver();
         webDriver.get("http://localhost:8080/");
-        randNum = Math.round(Math.random()*10000);
+		randNum = Math.round(Math.random() * 10000);
     }
 
     @Test
     public void aregistrationTest() throws InterruptedException {
-        webDriver.findElement(By.id("loginNavbar")).click();
-        synchronized (webDriver) {
-            webDriver.wait(800);
-            webDriver.findElement(By.id("signuptab")).click();
-            webDriver.wait(800);
-            webDriver.findElement(By.id("emailRegister")).sendKeys("test"+randNum+"@gmail.com");
-            webDriver.findElement(By.id("usernameRegister")).sendKeys("test"+randNum);
-            webDriver.findElement(By.id("nameRegister")).sendKeys("Test");
-            webDriver.findElement(By.id("surnameRegister")).sendKeys("Test");
-            webDriver.findElement(By.id("dateOfBirthRegister")).sendKeys(""+12122015);
-            webDriver.findElement(By.id("certMarkLoc")).sendKeys("Test");
-            webDriver.findElement(By.id("certExpirationDateRegister")).sendKeys(""+12122014);
-            webDriver.findElement(By.id("certWorkLangRegister")).sendKeys("estonian");
-            webDriver.findElement(By.id("passwordRegister")).sendKeys("Test"+randNum+"123");
-            webDriver.findElement(By.id("passwordRegister2")).sendKeys("Test"+randNum+"123");
-            webDriver.findElement(By.id("confirmsignup")).click();
-            webDriver.wait(600);
-            if (!webDriver.findElement(By.id("logoutNavbar")).getText().equalsIgnoreCase("log out")) { //ei olnud redirecti
-                webDriver.findElement(By.id("signintab")).click();
-                webDriver.wait(200);
-                webDriver.findElement(By.id("username")).sendKeys("test" + randNum);
-                webDriver.findElement(By.id("password")).sendKeys("Test" + randNum + "123");
-                webDriver.findElement(By.id("signinButton")).click();
-            }
-            webDriver.wait(1800);
-            webDriver.get("http://localhost:8080/giveAdmin");
-            webDriver.wait(800);
-            webDriver.get("http://localhost:8080");
-            webDriver.wait(800);
-            System.out.println(randNum);
-            Assert.assertTrue(webDriver.findElement(By.id("logoutNavbar")).getText() ,webDriver.findElement(By.id("logoutNavbar")).getText().equalsIgnoreCase("log out"));
-            webDriver.findElement(By.id("logoutNavbar")).click();
-        }
+		webDriver.findElement(By.id("loginNavbar")).click();
+		synchronized (webDriver) {			
+			webDriver.wait(800);
+			webDriver.findElement(By.id("signuptab")).click();
+			webDriver.wait(800);
+			webDriver.findElement(By.id("emailRegister")).sendKeys("test" + randNum + "@gmail.com");
+			webDriver.findElement(By.id("usernameRegister")).sendKeys("test" + randNum);
+			webDriver.findElement(By.id("nameRegister")).sendKeys("Test");
+			webDriver.findElement(By.id("surnameRegister")).sendKeys("Test");
+			webDriver.findElement(By.id("dateOfBirthRegister")).sendKeys("" + 12122015);
+			webDriver.findElement(By.id("certMarkLoc")).sendKeys("Test");
+			webDriver.findElement(By.id("certExpirationDateRegister")).sendKeys("" + 12122014);
+			webDriver.findElement(By.id("certWorkLangRegister")).sendKeys("estonian");
+			webDriver.findElement(By.id("passwordRegister")).sendKeys("Test" + randNum + "123");
+			webDriver.findElement(By.id("passwordRegister2")).sendKeys("Test" + randNum + "123");
+			webDriver.findElement(By.id("confirmsignup")).click();
+			webDriver.wait(500);
+			if (!webDriver.findElement(By.id("logoutNavbar")).getText().equalsIgnoreCase("log out")) { //ei olnud redirecti
+				webDriver.findElement(By.id("signintab")).click();
+				webDriver.wait(200);
+				webDriver.findElement(By.id("username")).sendKeys("test" + randNum);
+				webDriver.findElement(By.id("password")).sendKeys("Test" + randNum + "123");
+				webDriver.findElement(By.id("signinButton")).click();
+			}
+			webDriver.wait(1800);
+			webDriver.get("http://localhost:8080/giveAdmin");
+			webDriver.wait(800);
+			webDriver.get("http://localhost:8080");
+			webDriver.wait(800);
+			Assert.assertTrue(webDriver.findElement(By.id("logoutNavbar")).getText(), webDriver.findElement(By.id("logoutNavbar")).getText().equalsIgnoreCase("log out"));
+			
+		}
     }
 
     @Test
